511ff83fcd95674426013f3d7f8e77a055f01103
Argenis Osorio <argenisosorio580@hotmail.com>
2023-09-27 09:22:34 -0400

    feat: Agregados nuevos créditos

--- certificado.py
+++ certificado.py
@@ -17,6 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with certificado.py. If not, see <http://www.gnu.org/licenses/>.
 
+# Modificaciones del Ing. Argenis Osorio (argenisosorio10@gmail.com).
+
 import os
 import csv
 import time
